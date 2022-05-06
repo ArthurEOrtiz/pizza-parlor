c535ed015c5d9764d4b4e0925f3e97602e5c33b3
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-05-06 15:22:03 -0600

    pirce, toppings
    add code that will return the amount of toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
 //Buisness Logic
-
 function Pizza (toppings, sauce, size) {
   this.toppings = toppings;
   this.sauce = sauce;
   this.size = size;
-}
+};
 
-Pizza.prototype.price() {
-  console.LogPizza.toppings.length
-}
+Pizza.prototype.price = function() {
+  console.Log(Pizza.toppings.length);
+};
